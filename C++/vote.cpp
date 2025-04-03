18492e3f01bbaa1d726e4d2d70e8aea8e7d3119a
vijay62633 <ajaym6708@gmail.com>
2025-04-03 09:56:02 +0530

    3april

---
+++ C++/vote.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout<<"Enter your age:";
+  cin>>age;
+
+  if(age>=18){
+    cout<<"vote";
+  }
+  else if(age>=0 && age<18){
+    cout<<"not eligible to vote";
+  }
+  else{
+    cout<<"Invalid Input";
+  }
+
+return 0;
+}
