3f180e200093997b30c41131d5d0061158fee082
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-04-04 11:49:28 +0530

    4

---
+++ C++/password-check.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int pin;
+  cout<<"Enter your pin:";
+  cin>>pin;
+
+  if(pin==1234){
+    cout<<"Pin is correct.";
+  }
+  else{
+    cout<<"Pin is incorrect.";
+  }
+
+return 0;
+}
