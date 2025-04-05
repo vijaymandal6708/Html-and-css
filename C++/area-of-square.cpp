539ec80c727a938322f3ea047c908ed5e7e4bc4c
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-04-05 12:55:17 +0530

    5

---
+++ C++/area-of-square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int side;
+  cout<<"Enter value of side:";
+  cin>>side;
+
+  cout<<endl<<"Area of square is:"<<side*side;
+
+return 0;
+}
