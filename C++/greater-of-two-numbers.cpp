017b8598d6b1067da79d3e90f8ccf21d16a04e29
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-04-06 13:02:54 +0530

    6

---
+++ C++/greater-of-two-numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter first number:";
+  cin>>a;
+  cout<<"Enter second number:";
+  cin>>b;
+
+  if(a>b){
+    cout<<endl<<a<<" is greater";
+  }
+  else{
+    cout<<endl<<b<<" is greater";
+  }
+
+return 0;
+}
