057b358ec3806b0e6b0094fc40f3dea9647205ac
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-04-07 01:38:52 +0530

    7

---
+++ C++/sum-function-program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter first number:";
+  cin>>a;
+  cout<<"Enter second number:";
+  cin>>b;
+
+  int add(int a,int b){
+    return a+b;
+  }
+
+return 0;
+}
