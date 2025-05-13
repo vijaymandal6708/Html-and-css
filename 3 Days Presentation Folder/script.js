ea70fa67fdb8a617fb604d19fd1521793f5efe14
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-05-13 18:29:13 +0530

    13

---
+++ 3 Days Presentation Folder/script.js
@@ -0,0 +1,59 @@
+//let length = prompt("enter the length:");
+//let width = prompt("enter the width");
+
+//console.log(length*width);
+
+// spread operator
+// it copies a part of an array or a complete array to another array
+
+// const num1=[10,20,30,40];
+
+// const num2=[60,70,80,90,100];
+
+// const num3=[...num1, ...num2];
+
+// console.log(num3);
+
+// const arr=[1,2,3,4,5];
+
+// arr.push()
+// arr.unshift(0);
+// arr.reverse();
+
+// console.log(arr);
+
+// const person={
+//   name:"john",
+//   age:45,
+//   isEligible: true,
+//   adress: {
+//     state:"mp",
+//     country:"india"
+//   },
+//   hobbies:["dance","singing"]
+// }
+
+// console.log(person.name);
+// console.log(person['name']);
+// console.log(person.isEligible);
+// console.log(person.adress.country);
+// console.log(person.hobbies[1]);
+// person.greet();
+
+for(i=0; i<10; i++){
+  let a = prompt("enter value:");
+  console.log(a);
+}
+
+// let a = prompt("enter first value");
+// let b = prompt("enter second value");
+// let c = prompt("enter third value");
+// let d = prompt("enter fourth value");
+// let e = prompt("enter fif value");
+// let f = prompt("enter first value");
+// let g = prompt("enter first value");
+// let h = prompt("enter first value");
+// let i = prompt("enter first value");
+// let j = prompt("enter first value");
+
+// console.log(a);
