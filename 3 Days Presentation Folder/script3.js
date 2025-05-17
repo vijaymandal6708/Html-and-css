e804ffe02cc8ff1364689c72922bf9cd80384187
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-05-17 14:09:40 +0530

    17

--- 3 Days Presentation Folder/script3.js
+++ 3 Days Presentation Folder/script3.js
@@ -53,13 +53,106 @@ Demo();
 
 // wap to take a input from the user and print reverse table of that number
 
-function reverseTable(){
-  let a = prompt("Enter a number:");
-  for(let i=10; i<=a; i--){
-    a=a*i;
-    console.log(a);
-    console.log("");
-  }
+// function reverseTable(){
+//   let a = prompt("Enter a number:");
+//   for(let i=10; i>0; i--){
+//     console.log(a*i);
+//   }
+// }
+
+// reverseTable();
+
+// wap to take two inputs from the user and check 2 input even or odd , if number is even then print the table of 1 input and if number is odd then print area of circle of second function
+
+// types of function
+// 4 types
+
+// 1. no return no parameter
+
+function display1() {
+  console.log(10+20);
 }
 
-reverseTable(5);
+display1();
+
+// 2.return with no parameter
+function display2(){
+  return "hello";
+}
+
+let result = display2();
+console.log(result);
+// alert(result);
+
+// 3. parameter with no return
+function display3(x,y){
+  console.log(x+y);
+}
+
+display3(10,30);
+display3(34,56);
+
+// 4. parameter with return
+function display4(x,y){
+  return x*y;
+}
+
+let ans = display4(10,20);
+console.log(ans);
+
+
+// practice
+
+// function display5(){
+//   console.log(prompt("enter name"));
+// }
+
+// display5();
+
+// let name = prompt()
+
+function display6(){
+  return "goodafternoon";
+}
+
+let x= display6();
+console.log(x);
+
+function cube(x){
+  console.log(x*x*x);
+}
+
+cube(5);
+
+//object function
+
+function user1(person){
+  console.log(`person name is ${person.name} and age is ${person.age}`)
+}
+
+user1({
+  name:"john",
+  age:23,
+});
+
+// function expression
+
+let myfunction = function(){
+  console.log("this is a function expression")
+};
+ myfunction();
+
+ function subtraction(x,y){
+  console.log(x-y);
+ };
+
+ subtraction(5,3);
+
+ // arrow function
+
+ let myfunction2=() =>{
+   console.log("hello")
+
+ }
+
+ myfunction2();
