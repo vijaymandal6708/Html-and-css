a0f81a5f3cc8efe2ab3311da86e962793928a8b2
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-06-11 23:07:06 +0530

    11/06/25

--- JavaScript Folder/Stop-Watch-Project/stop.js
+++ JavaScript Folder/Stop-Watch-Project/stop.js
@@ -1,5 +1,25 @@
-let startBtn = document.querySelector('#start-btn');
-let stopBtn = document.querySelector('#start-btn');
-let resetBtn = document.querySelector('#start-btn');
+let startBtn = document.querySelector('.start-btn');
+let stopBtn = document.querySelector('.start-btn');
+let resetBtn = document.querySelector('.start-btn');
 
-let document
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
+
+let time = [0,0,0];
+let timerId = 0;
+
+startBtn.addEventListener("click",function(){
+  timerId = setInterval(() => {
+    time[2]++;
+    if(time[2]==60){
+      time[1]++;
+      time[2]=0;
+    }
+    if(time[1]==60){
+      time[0]++;
+      time[1]=0;
+    }
+    console.log(time);
+  }, 1000);
+})
