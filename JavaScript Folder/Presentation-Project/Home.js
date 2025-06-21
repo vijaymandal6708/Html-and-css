b95239fa3bcbf49f33398f86f0f7241f0c4831cd
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-06-21 18:28:37 +0530

    21/06/25

--- JavaScript Folder/Presentation-Project/Home.js
+++ JavaScript Folder/Presentation-Project/Home.js
@@ -1,22 +1,47 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.image');
+
+// -----------Browse by property type container---------
+
+const left1 = document.querySelector('.left1');
+const right1 = document.querySelector('.right1');
+const slider1 = document.querySelector('.slider1');
+const images1 = document.querySelectorAll('.image1');
 
 let slideNumber = 0;
 
-right.addEventListener("click", ()=>{
-  if(slideNumber<images.length-16){
+right1.addEventListener("click", ()=>{
+  if(slideNumber<images1.length-4){
     slideNumber++;
-    slider.style.transform = `translateX(-${slideNumber*326}px)`;
+    slider1.style.transform = `translateX(-${slideNumber*326}px)`;
   } 
 })
 
-left.addEventListener("click", ()=>{
+left1.addEventListener("click", ()=>{
   if(slideNumber>-3){
     slideNumber--;
-    slider.style.transform = `translateX(-${slideNumber*326}px)`;
+    slider1.style.transform = `translateX(-${slideNumber*326}px)`;
   } 
 })
 
 
+// --------Explore india container--------
+
+const left2 = document.querySelector('.left2');
+const right2 = document.querySelector('.right2');
+const slider2 = document.querySelector('.slider2');
+const images2 = document.querySelectorAll('.image2');
+
+slideNumber = 0;
+
+right2.addEventListener("click", ()=>{
+  if(slideNumber<6){
+    slideNumber++;
+    slider2.style.transform = `translateX(-${slideNumber*216}px)`;
+  }
+})
+
+left2.addEventListener("click", ()=>{
+  if(slideNumber>0){
+    slideNumber--;
+    slider2.style.transform = `translateX(-${slideNumber*216}px)`;
+  } 
+})
--- JavaScript Folder/Presentation-Project/crud.js
+++ JavaScript Folder/Presentation-Project/crud.js
@@ -1,13 +1,36 @@
 let FetchData=async()=>{
   
-let url='http://localhost:3000/hotel'
+let url='http://localhost:3000/hotel';
 
-let res= await fetch(url,{method:"GET"})
+let res= await fetch(url,{method:"GET"});
 
-let data=await res.json()
+let data=await res.json();
 
-console.log(data)
+let Show=document.querySelector("#display");
+
+data.map((e)=>{
+  Show.innerHTML+= `
+  
+  <tr>
+    <td>${e.name}</td>
+    <td>${e.number}</td>
+    <td>${e.city}</td>
+    <td>${e.age}</td>
+    <td>${e.id}</td>
+    <td onclick="Del('${e.id}')">Delete</td>
+
+  </tr>
+  
+  `
+})
 
 }
 
 FetchData();
+
+let Del=(id)=>{
+
+  let url = `http://localhost:3000/hotel/${id}`;
+  
+  fetch(url,{method:"DELETE"})
+}
