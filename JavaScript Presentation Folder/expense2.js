c1bd2c58cf11cc7dd48545d13469b6e0f3c757ac
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-02 18:44:31 +0530

    02/07/2025

--- JavaScript Presentation Folder/expense2.js
+++ JavaScript Presentation Folder/expense2.js
@@ -8,3 +8,16 @@ addbud.addEventListener("click", ()=>{
    console.log(inpbudget);
    balance.innerHTML=inpbudget;
 })
+
+
+
+let inpbudget=document.querySelector("#budget").value;
+// console.log(addbudget);
+let addbud=document.querySelector("#addbud");
+// console.log(addbudget);
+let balance = document.querySelector("#balance");
+
+addbud.addEventListener("click", ()=>{
+   console.log(inpbudget);
+   balance.innerHTML=inpbudget;
+})
