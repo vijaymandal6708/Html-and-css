07007dcabcfea8cbf5337ecf1d5639fbaf96873b
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-05 14:14:17 +0530

    05/07/25

--- Cybrom React/first_project/src/App.jsx
+++ Cybrom React/first_project/src/App.jsx
@@ -1,6 +1,16 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Services from "./components/Services"
+
 function App(){
   return(
-    <h1>Hello</h1>
+    <>
+       <h1>Hello</h1>
+       <h1>Hello</h1>
+       <About></About>
+       <Contact></Contact>
+       <Services></Services>
+    </>
   )
 }
 
---
+++ Cybrom React/first_project/src/components/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return(
+    <>
+       <h1>About</h1>
+
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet libero earum tenetur unde fuga assumenda doloribus eaque aut cumque voluptates deserunt quae recusandae laudantium, est ut sunt magni. Est, dolore!
+       Nulla hic autem quae optio mollitia suscipit quibusdam commodi corrupti labore, ipsum, itaque voluptate, reprehenderit numquam voluptates? Maxime facere a magni laboriosam earum, id, optio in alias nemo sint nobis?
+       Atque id inventore animi. Vero corporis iste voluptate ratione? Laudantium expedita voluptate iusto. Qui facilis placeat totam debitis itaque, dolores, quos sed neque quo aliquid corrupti voluptas veritatis quasi. Voluptas?
+       Accusantium laudantium molestias nulla expedita assumenda aliquam architecto voluptates velit? Tempore unde nesciunt explicabo, ducimus, cumque repellendus pariatur alias eveniet, molestias culpa dicta. Architecto labore dignissimos distinctio, quos doloremque quis?
+       Consequuntur rem repellat accusantium sapiente laborum cumque! Ad deleniti praesentium et sunt dignissimos odio ducimus quaerat ipsum doloremque accusamus quia autem omnis, maxime aliquid libero, dolor quos accusantium minima est.
+       Nemo sequi ratione molestias iusto itaque reiciendis ullam debitis dolorum velit, quae consequuntur aut animi dicta blanditiis cupiditate voluptatibus distinctio tempora consectetur maxime aliquam, iste pariatur. Impedit velit aliquid molestias!
+       Ipsam consequatur repellat corporis maxime officiis, veritatis, tempore quisquam obcaecati perferendis excepturi enim aliquid iure voluptatum alias non animi debitis explicabo nisi laudantium dicta iusto quia, nobis adipisci. Neque, numquam?
+       Ipsum deserunt, aut recusandae repellat eum molestias neque distinctio temporibus assumenda quo dolor nam fugit provident autem ex! Ipsum temporibus eius atque culpa fugit praesentium debitis! Autem praesentium excepturi sint.
+       Corporis doloremque dolores dolore amet, sint accusamus ea recusandae rem distinctio reprehenderit nam aliquam optio, ut eius explicabo, quam eveniet nihil ex quis illum officia animi. Quam praesentium doloremque dolores.
+       Reiciendis minus pariatur, quod ullam quae autem eos corrupti, ipsam enim facere amet molestias totam illo! Voluptates nesciunt amet fugit quaerat iste placeat in consectetur saepe, praesentium quisquam dicta deleniti?</p>
+
+
+    </>
+  )
+}
+
+
+export default About
