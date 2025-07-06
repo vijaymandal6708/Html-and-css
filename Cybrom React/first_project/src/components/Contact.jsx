3312b1cc74b43416a5be7d2a180fb3e982b0a03e
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-06 12:49:02 +0530

    06/07/25

--- Cybrom React/first_project/src/components/Contact.jsx
+++ Cybrom React/first_project/src/components/Contact.jsx
@@ -3,6 +3,7 @@ const Contact = () => {
     <>
       <h1>Contact</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis provident dolores? Voluptas ut, minima architecto deserunt ea autem laboriosam odio nobis, repudiandae libero excepturi asperiores aut, inventore dolorem officiis.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit reiciendis soluta, dicta libero voluptatibus facilis odit, perferendis porro nesciunt earum illo nam nobis sint tempore excepturi consequuntur fuga aliquam hic.</p>
     </>
   )
 };
