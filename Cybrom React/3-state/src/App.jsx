9f4c62b3cd43e16986be4c74ec3468a89ed78b7b
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-11 18:09:50 +0530

    11/07/25

---
+++ Cybrom React/3-state/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+function App(){
+
+  let [name,myfun] = useState(12)
+  let [age,setAge] = useState(12)
+  let [contact,setContact] = useState(1234)
+  let [number,setNumber] = useState(0)
+
+  function fun(){
+    myfun(90)
+  }
+
+  function fun2(){
+    setAge(89)
+  }
+
+
+  return(
+    <>
+      <h1>this is app - {name} {age}</h1>
+      <button onClick={fun}>Tap here</button>
+      <button onClick={fun2}>Change Age</button>
+
+      <h1>{contact}</h1>
+      <button onClick={()=>setContact(897)}>Change Contact</button>
+
+      <center><h1>{number}</h1></center>
+      <center><button onClick={()=>setNumber()}>+</button></center>
+      <center><button>-</button></center>
+    </>
+  )
+}
+
+export default App;
