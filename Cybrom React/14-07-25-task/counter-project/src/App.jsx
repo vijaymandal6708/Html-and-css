05a4b13e2e0d588ac04fb94def0b886218a895c3
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-16 18:13:39 +0530

    16/07/25

--- Cybrom React/14-07-25-task/counter-project/src/App.jsx
+++ Cybrom React/14-07-25-task/counter-project/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const Color = count%2 === 0 ? "lightblue" : "orange"
 
   return (
     <>
-      <center><h1>{count}</h1></center>
-      <center><button onClick={()=>setCount(count+1)}>+</button></center>
-      <br />
-      <center><button onClick={()=>setCount(count-1)}>-</button></center>
-      <br />
-      <center><button onClick={()=>setCount(0)}>Reset</button></center>
+    
+      <div className="container" style={{backgroundColor:Color, height:"250px", width:"500px", borderRadius:"15px", boxShadow:"3px 3px 5px 5px", display:"flex", justifyContent:"center", flexDirection:"column", justifyContent:"center", marginTop:"200px"}}>
+        <center><h1>{count}</h1></center>
+        <center><button onClick={()=>setCount(count+1)}>+</button></center>
+        <br />
+        <center><button onClick={()=>{
+          (count>0) ? setCount(count-1) : setCount(count=0)
+        }}>-</button></center>
+        <br />
+        <center><button onClick={()=>setCount(0)}>Reset</button></center>
+        </div>
 
     </>
   )
---
+++ Cybrom React/4-state-new/src/components/State.jsx
@@ -0,0 +1,12 @@
+function State(){
+  return(
+    <>
+    
+      let [sting, setSting] = useState("Cold drink")
+      {/* let [person, setPerson] = useState({names:"vijay"}) */}
+
+    </>
+  )
+}
+
+export default State
