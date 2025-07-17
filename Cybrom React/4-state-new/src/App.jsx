f48626af8af973ea32a841a476495e848701ded2
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-07-17 18:22:17 +0530

    17/07/25

--- Cybrom React/4-state-new/src/App.jsx
+++ Cybrom React/4-state-new/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import State from './components/State'
 
@@ -8,7 +5,6 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
       <State></State>
     </>
   )
--- Cybrom React/4-state-new/src/components/State.jsx
+++ Cybrom React/4-state-new/src/components/State.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
+
 function State(){
+
+  let [sting, setSting] = useState("Cold drink")
+      let [person,setPerson] = useState({
+        namee:"vijay", age:23
+      })
+
+      let [a,setA] = useState(false)
+
   return(
     <>
     
-      let [sting, setSting] = useState("Cold drink")
-      {/* let [person, setPerson] = useState({names:"vijay"}) */}
+      <div >
+
+        {/* <h1>What is the water type = {sting}</h1>
+        <h1>Person name is = {person.namee}</h1>
+        <h4>Vijay age is = {person.age}</h4> */}
+        
+        {a ? "" : <h1>Hide me</h1>}
+        <br />
+        
+        <button className="border p-3 rounded-2xl" onClick={()=>setA(true)}>Click here</button> &nbsp;&nbsp;&nbsp;&nbsp;
+        <button className="border p-3 rounded-2xl" onClick={()=>setA(false)}>Click here</button>
+        <br /> <br />
+        <button className="border p-3 rounded-2xl" onClick={()=>setA(!a)}>Toggle here</button>
+        <br /> <br />
+        <button className="border p-3 rounded-2xl" onClick={()=>setA(!a)}>{ a ? "Show" : "Hide"}</button>
+
+      </div>  
 
     </>
   )
