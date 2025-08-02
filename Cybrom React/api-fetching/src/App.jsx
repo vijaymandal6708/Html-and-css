78d9919d2114f7dfa9498b035fe093ff8e9b69f7
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-08-02 12:42:12 +0530

    02/08/25

--- Cybrom React/api-fetching/src/App.jsx
+++ Cybrom React/api-fetching/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Api from './components/Api'
 import {Route,Routes} from "react-router-dom"
+import Form from './components/Form'
 
 function App() {
 
--- Cybrom React/api-fetching/src/components/Form.jsx
+++ Cybrom React/api-fetching/src/components/Form.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+
 function Form(){
-   let [frmdata,setFrmdata] = useState
+
+   let [frmdata,setFrmdata] = useState({})
+
+   let nav = useNavigate()
+
+   function handleinput(e){
+      setFrmdata({...frmdata,[e.target.name]:e.target.value})
+   }
+
+   function finalsubmit(e){
+      e.preventDefault()
+      axios.post('http://localhost:3000/userdata',frmdata)
+      .then(()=>alert("inserted.."))
+      .catch((err)=>alert(err))
+
+      nav('./apidata')
+   }
+
+   return(
+      <>
+      
+        <form action="" onSubmit={finalsubmit} align="center" style={{marginTop:"200px"}}>
+
+          <label htmlFor="">Name:</label>
+          <input type="text" name="name" onChange={handleinput}/> <br /> <br />
+
+          <label htmlFor="">Contact:</label>
+          <input type="text" name="contact" onChange={handleinput}/> <br /> <br />
+
+          <label htmlFor="">City:</label>
+          <input type="text" name="city" onChange={handleinput}/> <br /> <br />
+
+          <label htmlFor="">Email:</label>
+          <input type="text" name="email" onChange={handleinput}/> <br /> <br />
+
+          <input type="submit"/>
+
+        </form>
+      
+      </>
+   )
 }
+
+export default Form
