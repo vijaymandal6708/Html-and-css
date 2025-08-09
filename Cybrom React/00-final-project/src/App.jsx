1050dc08cf20bd0d52dd8f80ef6cdb0d318f391b
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-08-09 12:45:36 +0530

    09/08/25

--- Cybrom React/00-final-project/src/App.jsx
+++ Cybrom React/00-final-project/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   return (
     <>
 
-       <nav className="bg-[rgb(32,167,229)] p-[8px] px-8">
+       <nav className="bg-[rgb(32,167,229)] p-[8px] px-8 flex gap-[30px] items-center text-white font-[650]">
          <div className="logo h-[6vh] w-[12vw]">
-          <img src="logo.jpg" alt="" className="bg-no-repeat bg-contain h-[6vh] w-[12vw]"/>
+          <img src="logo.jpg" alt="" className="bg-no-repeat bg-contain h-[6vh] w-[12vw] "/>
          </div>
-         <div className="search-bar"></div>
-         <div className="cart-container"></div>
-         <div className="wishlist-container"></div>
-         <div className="login-container"></div>
+         <div className="categories ml-[300px]">Latest Products</div>
+         <div className="wishlist-container">Wishlist</div>
+         <div className="cart-container">Cart</div>
+         <div className="login-container">Login</div>
        </nav>
 
     </>
