75a217ebbbc1afdd4f7d4d0e73fe80f11329ba2e
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-08-31 13:31:28 +0530

    31/08/25

---
+++ Cybrom React/Presentation Folder/calculator-project/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  let [val, setVal] = useState("");
+
+  const operators = ["+", "-", "*", "/", "%", "."];
+
+  function append(input) {
+    // Prevent two operators in a row
+    if (operators.includes(input) && operators.includes(val.slice(-1))) {
+      return; // do nothing
+    }
+    setVal(val + input);
+  }
+
+  function calculate() {
+    try {
+      setVal(eval(val).toString());
+    } catch {
+      setVal("Error");
+    }
+  }
+
+  function clearr() {
+    setVal(val.slice(0, -1));
+  }
+
+  function allclear() {
+    setVal("");
+  }
+
+  return (
+    <>
+      <h2>Calculator Project</h2>
+      <div className="container">
+        <input type="text" value={val} id="display" disabled />
+        <div className="button-container">
+          <button className='button' onClick={allclear}>AC</button>
+          <button className='button' onClick={clearr}>DEL</button>
+          <button className='button' onClick={() => append("%")}>%</button>
+          <button className='button' onClick={() => append("/")}>/</button>
+          <button className='button' onClick={() => append("7")}>7</button>
+          <button className='button' onClick={() => append("8")}>8</button>
+          <button className='button' onClick={() => append("9")}>9</button>
+          <button className='button' onClick={() => append("*")}>*</button>
+          <button className='button' onClick={() => append("4")}>4</button>
+          <button className='button' onClick={() => append("5")}>5</button>
+          <button className='button' onClick={() => append("6")}>6</button>
+          <button className='button' onClick={() => append("-")}>-</button>
+          <button className='button' onClick={() => append("1")}>1</button>
+          <button className='button' onClick={() => append("2")}>2</button>
+          <button className='button' onClick={() => append("3")}>3</button>
+          <button className='button' onClick={() => append("+")}>+</button>
+          <button className='button' onClick={() => append("00")}>00</button>
+          <button className='button' onClick={() => append("0")}>0</button>
+          <button className='button' onClick={() => append(".")}>.</button>
+          <button className='button equal' onClick={calculate}>=</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
