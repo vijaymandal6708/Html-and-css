d6ebecd378b75e6cfc2200367a1aa77cd1309ef9
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-09-10 16:26:37 +0530

    10/09/25

--- 00NodeJs/newindex.js
+++ 00NodeJs/newindex.js
@@ -2,8 +2,8 @@ const http = require("http");
 const MyModule = require("./newcybrom");
 
 http.createServer((req,res)=>{
-    res.write(MyModule.MyDate);
-    res.write(MyModule.MyCity);
+    res.write(MyModule.MyDate());
+    res.write(MyModule.MyCity());
     res.end("server end");
 }).listen(8500, ()=>{
     console.log("server run on port 8500");
