b3c660fc87f8d4d34d0232210e2914849889b8eb
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-09-13 13:14:18 +0530

    13/09/25

---
+++ 00NodeJs/05 - Fs Module/index9.js
@@ -0,0 +1,10 @@
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require("path");
+
+// const mypath = path.join(__dirname, "raj.txt");
+
+const mypath = path.join(__dirname, "Photos", "images", "raj.txt");
+console.log(mypath);
+
